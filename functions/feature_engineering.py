f416428c56d06d1e19ddf67241f7bf0e8aac7769
Valdenilson Maximino <vmaximinocf@gmail.com>
2025-11-29 17:10:43 -0300

    Arquivo de processamento V2

---
+++ functions/feature_engineering.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def feature_engineering(df):
+    # ID_portador column
+    df["ID_portador"] = df["CPF PORTADOR"] + df["NOME PORTADOR"]
+
+    # Weekend column (including "SIGILOSO" rule)
+    df["fim_semana"] = np.where(
+        df['SIGILOSO'] == 1,
+        0,
+        df['DATA TRANSAÇÃO'].dt.day_of_year.isin([5, 6]).astype(int)
+    )
+
+    # Transform value into log
+    df['LOG_VALOR'] = np.log1p(df['VALOR TRANSAÇÃO'])
+
+    return df
