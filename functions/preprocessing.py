daf05a9b737199c72f71c300d4519ed37ecb940b
julia almeida <juliaalmeida1102@gmail.com>
2025-11-29 15:18:55 -0300

    feat: alterei o notebook de features para pasta correta e atualizei o script de preprocessing

--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -16,44 +16,30 @@ cat_columns = [
     'ID_PORTADOR', 'CNPJ OU CPF FAVORECIDO'
 ]
 
-# Numerical Value Column (Only Scaling, as Log is pre-calculated)
-num_value = ['LOG_VALOR']
-
-# Other Numerical Columns (Scaling for codes, frequencies, and ratios)
-num_other = [
-    'CÓDIGO ÓRGÃO SUPERIOR', 'CÓDIGO ÓRGÃO', 'CÓDIGO UNIDADE GESTORA', 'ANO EXTRATO',
-    'MÊS EXTRATO', 'SIGILOSO', 'FIM_SEMANA', 'FREQ_NOME ÓRGÃO',
-    'FREQ_ESTADO_ESTIMADO', 'FREQ_NOME FAVORECIDO', 'SEMESTRE_EXTRATO',
-    'MEDIA_VALOR_ORGAO_MES', 'MEDIA_VALOR_ORGAO_SEM', 'RATIO_MES', 'RATIO_SEM'
+# Numerical Columns (Scaling for codes, frequencies, and ratios)
+num_value = ['LOG_VALOR','ANO EXTRATO','MÊS EXTRATO', 'SIGILOSO', 'FIM_SEMANA', 'FREQ_NOME ÓRGÃO',
+    'FREQ_ESTADO_ESTIMADO', 'FREQ_NOME FAVORECIDO','MEDIA_VALOR_ORGAO_MES','RATIO_MES'
 ]
 
 
 # 2. Define Individual Pipelines (Transformers)
 
-# Numerical other pipeline: Robust Scaling for codes and ratios.
-num_other_pipeline = RobustScaler()
-
 # Numerical value pipeline: Robust Scaling for the already log-transformed value.
 num_value_pipeline = RobustScaler()
 
 # Categorical pipeline: Impute missing categories with 'Desconhecido', then One-Hot Encode.
-cat_pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy='constant', fill_value='Desconhecido')),
-    ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
-])
+#cat_pipeline = Pipeline([
+#    ('imputer', SimpleImputer(strategy='constant', fill_value='Desconhecido')),
+#    ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False)) - Comentado para evitar alta dimensionalidade
+#])
 
 
-# 3. Define the ColumnTransformer 
+# 3. Define the ColumnTransformer
 
 # The final preprocessor object that combines all steps.
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num_other', num_other_pipeline, num_other),
-        ('num_value', num_value_pipeline, num_value),
-        ('cat', cat_pipeline, cat_columns),
-    ],
-    remainder='passthrough'
-)
+        ('num_value', num_value_pipeline, num_value)], remainder='drop')
 
 # You can also define a function to load the combined preprocessor object if needed:
 def get_preprocessor():
